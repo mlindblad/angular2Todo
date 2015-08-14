51373c2161cec8c2b976ee97e3bcdffee4c7fb1a
Martin Lindblad <martin.lindblad@find-out.se>
2015-08-14 17:06:17 +0200

    Work in progress

--- build/todo.js
+++ build/todo.js
@@ -11,13 +11,48 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var angular2_1 = require("angular2/angular2");
+var angularfire_1 = require("firebase/angularfire");
 var TodoApp = (function () {
-    function TodoApp() {
+    function TodoApp(angularFire) {
+        this.todoService = angularFire.asArray();
+        this.todoEdit = null;
     }
+    TodoApp.prototype.addTodoItem = function ($event, newTodo) {
+        if ($event.which == 13) {
+            this.todoService.add({
+                name: newTodo.value,
+                completed: false
+            });
+            newTodo.value = '';
+        }
+    };
+    TodoApp.prototype.toogleTodoItem = function (todo) {
+        console.log(todo);
+        todo.completed = !todo.completed;
+        this.todoService.save(todo);
+    };
+    TodoApp.prototype.listTodos = function () {
+        return this.todoService.list;
+    };
+    TodoApp.prototype.deleteTodo = function (todo) {
+        this.todoService.remove(todo);
+    };
+    TodoApp.prototype.editTodo = function (todo) {
+        this.todoEdit = todo;
+    };
     TodoApp = __decorate([
-        angular2_1.Component({ selector: 'todo-app' }),
-        angular2_1.View({ templateUrl: 'todo.html' }), 
-        __metadata('design:paramtypes', [])
+        angular2_1.Component({
+            selector: 'todo-app',
+            appInjector: [
+                angularfire_1.AngularFire,
+                angular2_1.bind(Firebase).toValue(new Firebase('https://l90gyovhjiw.firebaseio-demo.com/'))
+            ]
+        }),
+        angular2_1.View({
+            templateUrl: 'todo.html',
+            directives: [angular2_1.NgFor]
+        }), 
+        __metadata('design:paramtypes', [angularfire_1.AngularFire])
     ], TodoApp);
     return TodoApp;
 })();
--- todo.ts
+++ todo.ts
@@ -1,11 +1,57 @@
 /// <reference path="typings/angular2/angular2"/>
 
-import {Component, bootstrap, View} from "angular2/angular2";
+import {Component, bootstrap, View, bind, NgFor} from "angular2/angular2";
+import {AngularFire, FirebaseArray} from "firebase/angularfire"
 
-@Component({selector: 'todo-app'})
-@View({templateUrl: 'todo.html'})
+@Component({
+	selector: 'todo-app',
+	appInjector: [
+    	AngularFire,
+    	bind(Firebase).toValue(new Firebase('https://l90gyovhjiw.firebaseio-demo.com/'))
+  	]
+})
+@View({
+	templateUrl: 'todo.html',
+	directives: [NgFor]
+	
+	})
 class TodoApp {
 	
+	todoService: FirebaseArray;
+	todoEdit: any;
+	
+	constructor(angularFire: AngularFire) {
+		this.todoService = angularFire.asArray();
+		this.todoEdit = null;
+	}
+	
+	addTodoItem($event, newTodo) {
+		if($event.which == 13) {
+			this.todoService.add({
+				name: newTodo.value,
+				completed: false
+			});
+			newTodo.value = '';
+		}
+	}
+	
+	toogleTodoItem(todo) {
+		console.log(todo);
+		todo.completed = !todo.completed
+		this.todoService.save(todo);
+	}
+	
+	listTodos() {
+		return this.todoService.list;
+	}
+	
+	deleteTodo(todo) {
+		this.todoService.remove(todo);
+	}
+	
+	editTodo(todo) {
+		this.todoEdit = todo;
+	}
 }
 
 bootstrap(TodoApp);
